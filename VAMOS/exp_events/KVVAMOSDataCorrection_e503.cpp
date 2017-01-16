ec29ab384bfac60506c973c3ea83007290272756
Quentin Fable <quentin.fable@ganil.fr>
2017-01-16 11:43:18 +0100

    Add specific class correction for e503

---
+++ VAMOS/exp_events/KVVAMOSDataCorrection_e503.cpp
@@ -0,0 +1,30 @@
+//Created by KVClassFactory on Mon Jan 16 11:36:31 2017
+//Author: Quentin Fable,,,
+
+#include "KVVAMOSDataCorrection_e503.h"
+
+ClassImp(KVVAMOSDataCorrection_e503)
+
+////////////////////////////////////////////////////////////////////////////////
+// BEGIN_HTML <!--
+/* -->
+<h2>KVVAMOSDataCorrection_e503</h2>
+<h4>A KVVAMOSDataCorrection derived class specific to e503 experiment</h4>
+<!-- */
+// --> END_HTML
+////////////////////////////////////////////////////////////////////////////////
+
+KVVAMOSDataCorrection_e503::KVVAMOSDataCorrection_e503()
+{
+   // Default constructor
+}
+
+//____________________________________________________________________________//
+
+KVVAMOSDataCorrection_e503::~KVVAMOSDataCorrection_e503()
+{
+   // Destructor
+}
+
+//____________________________________________________________________________//
+
---
+++ VAMOS/exp_events/KVVAMOSDataCorrection_e503.h
@@ -0,0 +1,18 @@
+//Created by KVClassFactory on Mon Jan 16 11:36:31 2017
+//Author: Quentin Fable,,,
+
+#ifndef __KVVAMOSDATACORRECTION_E503_H
+#define __KVVAMOSDATACORRECTION_E503_H
+
+#include "KVVAMOSDataCorrection.h"
+
+class KVVAMOSDataCorrection_e503 : public KVVAMOSDataCorrection {
+public:
+   KVVAMOSDataCorrection_e503();
+
+   virtual ~KVVAMOSDataCorrection_e503();
+
+   ClassDef(KVVAMOSDataCorrection_e503, 1) //A KVVAMOSDataCorrection derived class specific to e503 experiment
+};
+
+#endif
