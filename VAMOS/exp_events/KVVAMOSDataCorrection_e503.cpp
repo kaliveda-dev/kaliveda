3c99a869188cedfd49c19c6b5d7d02c70dbcb433
Quentin Fable <quentin.fable@ganil.fr>
2017-06-09 14:28:32 +0200

    je sais plus

--- VAMOS/exp_events/KVVAMOSDataCorrection_e503.cpp
+++ VAMOS/exp_events/KVVAMOSDataCorrection_e503.cpp
@@ -26,7 +26,7 @@ ClassImp(KVVAMOSDataCorrection_e503)
 KVVAMOSDataCorrection_e503::KVVAMOSDataCorrection_e503(Int_t run_number = -1) : KVVAMOSDataCorrection(run_number)
 {
    // Default constructor
-   fkverbose   = kTRUE;
+   fkverbose   = kFALSE;
    fkIsInit    = kFALSE;
 
    // HF frequency correction
