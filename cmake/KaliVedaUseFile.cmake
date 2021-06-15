6784bd22b4ad4751fe2b4c7d74dc9be5cfe7134b
John Frankland <john.frankland@ganil.fr>
2021-06-15 12:26:11 +0200

    enforce std=c++11 in dependent projects if necessary
     * all projects using KaliVeda need to use cmake recent
       with CMAKE_CXX_STANDARD and CMAKE_CXX_STANDARD_REQUIRED

--- cmake/KaliVedaUseFile.cmake
+++ cmake/KaliVedaUseFile.cmake
@@ -7,3 +7,9 @@ include_directories(${KALIVEDA_INCLUDE_DIR})
 #---to be able to use modules for building with ROOT 5 or 6
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH}  ${CMAKE_CURRENT_LIST_DIR})
 
+#---do we need to enforce C++11 standard in order to be able to compile with KaliVeda?
+if(KaliVeda_NEEDS_CXX11)
+    set(CMAKE_CXX_STANDARD 11)
+    set(CMAKE_CXX_STANDARD_REQUIRED yes)
+    message(STATUS "KaliVeda: C++11 support is required and has been enabled")
+endif(KaliVeda_NEEDS_CXX11)
